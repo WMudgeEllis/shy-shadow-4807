7e34b97f000213e06e1c8c826c224f0a1a46e4d9
Weston Ellis <ellisweston112@gmail.com>
2021-09-22 10:21:09 -0600

    Write model test

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe Plant do
     it { should have_many(:plots).through(:plot_plants) }
   end
 
+  it 'can fetch the plot_plant' do
+    garden = Garden.create!(name: 'awesome space', organic: true)
+    plot_1 = garden.plots.create!(number: 35, size: 'Bigly', direction: 'up')
+    plot_2 = garden.plots.create!(number: 20, size: 'Smally', direction: 'down')
 
+    plant_1 = Plant.create!(name: 'shrubbery', description: 'is in awfully high demand', days_to_harvest: 200)
+
+    plot_2.plants << plant_1
+
+    plot_plant = PlotPlant.create!(plot: plot_1, plant: plant_1)
+
+    expect(plant_1.fetch_plot_plant).to eq(plot_plant)
+  end
 end
