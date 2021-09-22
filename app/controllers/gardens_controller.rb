527e8ab84b35eb1744163cfd4f8e8780289a2cd4
Weston Ellis <ellisweston112@gmail.com>
2021-09-22 11:08:56 -0600

    Finish story 3

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -2,7 +2,7 @@ class GardensController < ApplicationController
 
   def show
     garden = Garden.find(params[:id])
-    @plants_distinct = garden.distinct_plants
+    @plants_distinct_short_harvest_time = garden.plants_with_short_harvest_times
   end
 
 end
