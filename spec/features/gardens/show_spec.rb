a5d82fa7ee4b7750eccc057c701a3fb198966704
Weston Ellis <ellisweston112@gmail.com>
2021-09-22 11:24:21 -0600

    Write extension test

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe 'garden show page' do
 
     plant_1 = Plant.create!(name: 'shrubbery', description: 'is in awfully high demand', days_to_harvest: 20)
     plant_2 = Plant.create!(name: 'bush', description: 'is not in high demand', days_to_harvest: 50)
-    plant_3 = Plant.create!(name: 'apple tree', description: 'is a tree', days_to_harvest: 100)
+    plant_3 = Plant.create!(name: 'apple tree', description: 'is a tree', days_to_harvest: 101)
 
     plot_1.plants << plant_1
     plot_1.plants << plant_3
@@ -50,5 +50,26 @@ RSpec.describe 'garden show page' do
     expect(page).to have_content(plant_1.name)
     expect(page).to have_content(plant_2.name)
     expect(page).to_not have_content(plant_3.name)
+  end
+
+  it 'can order by number of times planted' do
+    garden = Garden.create!(name: 'awesome space', organic: true)
+    plot_1 = garden.plots.create!(number: 35, size: 'Bigly', direction: 'up')
+    plot_2 = garden.plots.create!(number: 20, size: 'Smally', direction: 'down')
+
+    plant_1 = Plant.create!(name: 'shrubbery', description: 'is in awfully high demand', days_to_harvest: 20)
+    plant_2 = Plant.create!(name: 'bush', description: 'is not in high demand', days_to_harvest: 50)
+    plant_3 = Plant.create!(name: 'apple tree', description: 'is a tree', days_to_harvest: 99)
+
+    plot_1.plants << plant_1
+    plot_1.plants << plant_3
+
+    plot_2.plants << plant_2
+    plot_2.plants << plant_3
+
+    visit garden_path(garden)
+
+    expect(plant_3.name).to appear_before(plant_1.name)
+    expect(plant_3.name).to appear_before(plant_2.name)
   end
 end
