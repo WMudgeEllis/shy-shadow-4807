914152669678acc36b44fa90f09759077834e2f8
Weston Ellis <ellisweston112@gmail.com>
2021-09-22 11:06:26 -0600

    Write model query

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -5,4 +5,8 @@ class Garden < ApplicationRecord
   def distinct_plants
     plants.distinct
   end
+
+  def plants_with_short_harvest_times
+    distinct_plants.where('plants.days_to_harvest < ?', '100')
+  end
 end
