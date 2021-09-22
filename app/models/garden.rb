b129da33a7327c664fa7b62f2e86006b96e82fff
Weston Ellis <ellisweston112@gmail.com>
2021-09-22 11:19:58 -0600

    Write query

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -9,4 +9,10 @@ class Garden < ApplicationRecord
   def plants_with_short_harvest_times
     distinct_plants.where('plants.days_to_harvest < ?', '100')
   end
+
+  def order_by_occurrance
+    plants.where('plants.days_to_harvest < ?', '100')
+          .group(:id)
+          .order('COUNT(plants.id) DESC')
+  end
 end
